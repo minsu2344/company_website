906c31c3efd6e463057c95c15fa0f999ca9bdd75
minsoo <tksemf1706@naver.com>
2025-03-16 23:27:10 +0900

    feat: navbar 추가

---
+++ frontend/src/Components/Navbar/NavBar.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom"
+
+
+export default function NavBar() {
+  interface MenuItem {
+    path: string;
+    label: string;
+  }
+
+  const menuItems: MenuItem[] = [
+    {
+      path: "/", label: '홈'
+    },
+    {
+      path: "/about", label: '회사정보'
+    },
+    {
+      path: "/leadership", label: '임원소개'
+    },
+    {
+      path: "/board", label: '업무게시판'
+    },
+    {
+      path: "/our-services", label: '제공기술'
+    },
+    {
+      path: "/contact", label: '문의하기'
+    },
+  ]
+
+  return (
+    <nav className='fixed top-0 left-0 w-full z-50 bg-white text-black p-4 shadow-lg'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <h1 className='text-xl lg:text-2xl font-bold lg:ml-12 lg:mr-8'>
+          <Link to="/">MS Company</Link>
+        </h1>
+
+        <div className='lg:flex flex-1 justify-center'>
+          <ul className='flex gap-8 text-lg'>
+            {menuItems.map((item) => {
+              return (<li key={item.path}>
+                <Link to={item.path} className="hover:text-blue-600 transition duration-300">{item.label}</Link>
+              </li>)
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
