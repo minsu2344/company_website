2eb612351d048c9e79d37e564b88fc96720aa7e1
minsoo <tksemf1706@naver.com>
2025-03-23 22:35:32 +0900

    feat: mongoDB 연결

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('안녕');
-})
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB 연결 성공'))
+  .catch((err) => console.log(`DB 연결 실패: ${err}`));
 
 app.listen(PORT, () => {
-  console.log('서버 도는 중')
-})
+  console.log('서버 도는 중');
+});
